20a900478a98f01495ed7534899f4bdf400c61c6
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-10-27 13:11:44 +0530

    Login Fix

--- app/component/Sessionwrapper.js
+++ app/component/Sessionwrapper.js
@@ -2,9 +2,13 @@
 import React from 'react'
 import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs"
 const Sessionwrapper = ({children}) => {
+  
   return (
     <div>
-      <KindeProvider>
+      <KindeProvider clientId={process.env.KINDE_CLIENT_ID} // your prod client id
+      redirectUri={process.env.NEXT_PUBLIC_KINDE_REDIRECT_URI} // https://civic-pulse-eight.vercel.app/api/auth/kinde_callback
+      issuer={process.env.KINDE_ISSUER_URL} // https://webdev1717.kinde.com
+      siteUrl={process.env.NEXT_PUBLIC_KINDE_SITE_URL}>
         {children}
       </KindeProvider>
     </div>
