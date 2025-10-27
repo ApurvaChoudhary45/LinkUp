6984eb204e214d36e238561798a5839fa0d3d9e3
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-10-27 16:54:19 +0530

    Bug Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com'],
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "files.edgestore.dev",
-      
-    },
-  ],
-},
-};
+const nextConfig = {};
 
 export default nextConfig;
